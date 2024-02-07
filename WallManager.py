dfc9205497786405dadd3c860db7a0cb0bbe82a9
rosrunner <luca.alfano@uni-a.de>
2024-02-07 16:49:43 +0100

    added more repos

--- WallManager.py
+++ WallManager.py
@@ -16,101 +16,37 @@ class WallManager(AbstractVirtualCapability):
     def __init__(self, server):
         super().__init__(server)
         self.wall = []
-        self.car = None
+        self.cars = []
+        self.copter_swarm = None
+        self.block_handler = None
         self.blocks = []
 
+    def tick(self, params: dict):
+        copter = self.invoke_sync("GetAvaiableCopter", params)["Device"]
 
-    def LoadBuildPlan(self, params: dict):
-        file_location = params["SimpleStringParameter"]
-        formatPrint(self, f"Loading Build Plan: {file_location}")
-        if os.path.exists(file_location):
-            with open(file_location, mode='r') as file:
-                self.build_plan = json.loads(file.read())
-                self.max_key = max([int(k) for k in self.build_plan.keys()])
-        else:
-            raise FileNotFoundError("This file does not exist")
-        formatPrint(self, f"New BuildPlan: {self.build_plan}")
-        return {}
 
-    def GetNextBlockPosition(self, params: dict):
-        for i in range(1, self.max_key):
-            key = str(i)
-            if key not in self.fitted_blocks:
-                dependency_resolved = True
-                for dependency in self.build_plan[key]["depends_on"]:
-                    dependency_resolved &= str(dependency) in self.fitted_blocks
-                if not dependency_resolved:
-                    continue
-                pos = np.round(np.array(self.build_plan[key]["position"]), decimals=5)
-                rot = np.round(np.array(self.build_plan[key]["rotation"]), decimals=7)
+        return params
 
-                ret = {"Position3D": pos.tolist(), "Quaternion": rot.tolist(),
-                       "Vector3": self.build_plan[key]["shape"]}
-                self.fitted_blocks += [key]
-                return ret
-        raise ValueError("No Block avaiable")
+    def GetSwarm(self, params: dict):
+        return {"Device": self.copter_swarm}
 
-    def GetAvailableBlocks(self, params: dict):
-        blocks = []
-        for i in range(1, self.max_key):
-            key = str(i)
-            if key not in self.fitted_blocks:
-                dependency_resolved = True
-                for dependency in self.build_plan[key]["depends_on"]:
-                    dependency_resolved &= str(dependency) in self.fitted_blocks
-                if not dependency_resolved:
-                    continue
-                pos = np.round(np.array(self.build_plan[key]["position"]), decimals=5) * self.factor
-                rot = np.round(np.array(self.build_plan[key]["rotation"]), decimals=7)
+    def SetSwarm(self, params: dict):
+        self.copter_swarm = json.loads(params["Device"])
+        return self.GetSwarm({})
 
-                ret = {"Position3D": pos.tolist(), "Quaternion": rot.tolist(),
-                       "Vector3": (np.array(self.build_plan[key]["shape"]) * self.factor).tolist(), "int": key}
-                blocks.append(ret)
-        for b in blocks:
-            self.fitted_blocks += [b["int"]]
-        return {"ParameterList": blocks}
+    def GetBlockHandler(self, params: dict):
+        return {"Device": self.block_handler}
 
-    # noinspection PyUnreachableCode
-    def GetWalls(self, params: dict) -> dict:
-        walls = []
-        for i in range(1, self.max_key):
-            key = str(i)
-            block = self.build_plan[key]
-            pos = np.array(block["position"]) * self.factor
-            rotation = block["rotation"]
+    def SetBlockHandler(self, params: dict):
+        self.block_handler = json.loads(params["Device"])
+        return self.GetBlockHandler({})
 
-            quat = quaternion.as_quat_array(rotation)
-            x = quaternion.rotate_vectors(quat, np.array([1., 0., 0.]))
-            y = quaternion.rotate_vectors(quat, np.array([0., 1., 0.]))
-            z = quaternion.rotate_vectors(quat, np.array([0., 0., 1.]))
-            norm = np.cross(x, z)
+    def GetBlocks(self, params: dict) -> dict:
+        return {"blocks": self.blocks}
 
-            d = np.dot(np.array(pos), norm)
-            norm_0 = norm / np.sum(np.sqrt(norm ** 2))
-            norm_0 *= -1 if d < 0 else 1
-            norm_0 = np.array(np.round(norm_0, decimals=5))
-            d = np.round(np.dot(np.array(pos), norm_0), decimals=5)
-            # print(f"Pos  {pos} ,Norm  {norm_0} D {d}")
-            wall = norm_0.tolist()
-            wall.append(d)
-            # Add rotation to list (should be equal over all blocks)
-            rot = np.round(np.array(rotation), decimals=5).tolist()
-            wall += rot
-            pseudo_walls = [w[:4] for w in walls]
-            if wall[:4] not in pseudo_walls:
-                walls.append(wall)
-        return {"ListOfPoints": walls}
-
-    # noinspection PyUnreachableCode
-    def GetStartingPoints(self, params: dict):
-        walls = self.GetWalls(params)
-        points = []
-        for wall in walls["ListOfPoints"]:
-            points.append(
-                (np.array(wall[:3]) * wall[3]).tolist() + (np.cross(wall[:3], [0, 0, 1]) != 0).astype(
-                    float).tolist() + wall[4:8])
-
-        return {"ListOfPoints": points}
+    def SetBlocks(self, params: dict) -> dict:
+        self.blocks = params["blocks"]
+        return self.GetBlocks({})
 
     def loop(self):
         sleep(.0001)
@@ -122,7 +58,6 @@ if __name__ == '__main__':
         print("[Main] Received SIGTERM signal")
         listener.kill()
         quit(1)
-
 
     try:
         port = None
